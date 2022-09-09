dff0030d99e90ec08b5feeea91039e9b3191e39d
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-09-09 10:21:16 +0430

    Authenticate and Set path after signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!, except: :welcome
+    
+    def after_sign_in_path_for(groups)
+      groups_path        
+    end
+    
 end
