c3198753d6ca42d9dba71f99fcce22fafc3d765d
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-09-09 15:39:46 +0430

    Add authorizations

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, User
+
+    return unless user.present?
+
+    can :manage, Group, user: user
+    can :manage, Expense, user: user
+    can :manage, User, user: user
+  end
+end
