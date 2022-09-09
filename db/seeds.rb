556375ab1a754b4cac08b041f2bbbab7c47839c7
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-09-09 10:19:26 +0430

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+@user1 = User.create(name: "Ali", email: "ali@gmail.com", password: "123456", password_confirmation: "123456")
+@user2 = User.create(name: "Mahdi", email: "mahdi@gmail.com", password: "123456", password_confirmation: "123456")
+
+@group1 = Group.create(user_id: @user1.id, name: "Travel", icon: "✈️")
+@group2 = Group.create(user_id: @user1.id, name: "Food", icon: "🍔")
+@group3 = Group.create(user_id: @user1.id, name: "Study", icon: "📚")
+
+@expense1 = Expense.create(user_id: @user1.id, name: "India", amount: 500)
+@expense2 = Expense.create(user_id: @user1.id, name: "Bullani", amount: 10)
+@expense3 = Expense.create(user_id: @user1.id, name: "America", amount: 10000)
+
+@group_expense1 = GroupExpense.create(group_id:  @group1.id, expense_id: @expense1.id) 
+@group_expense2 = GroupExpense.create(group_id:  @group2.id, expense_id: @expense2.id) 
+@group_expense3 = GroupExpense.create(group_id:  @group1.id, expense_id: @expense3.id) 
