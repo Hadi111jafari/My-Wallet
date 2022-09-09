9a223cd245758a10d294360ba1a23cf98d8dd44a
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-09-09 10:20:10 +0430

    Create controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+    def index
+        @user = User.where(user_id: current_user.id)
+    end
+
+    def welcome
+    end 
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation )
+    end
+end
