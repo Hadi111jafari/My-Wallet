6b538cb68f03df32950aaf133404959681815fb5
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-09-09 10:25:15 +0430

    Add tests for models

---
+++ spec/models/group_expense_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe GroupExpense, type: :model do
+  subject { GroupExpense.new(group_id: 1, expense_id: 1)}
+
+  before { subject.save }
+
+  it 'group_id should be present' do
+    subject.group_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'expense_id should be present' do
+    subject.expense_id = nil
+    expect(subject).to_not be_valid
+  end
+end
