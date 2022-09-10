c146ea90933e98dadc2b3bb37f86d49efa50302d
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-09-10 08:53:17 +0430

    Formate code with rubocop

--- spec/requests/expenses_controllers_spec.rb
+++ spec/requests/expenses_controllers_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "/expenses_controllers", type: :request do
+RSpec.describe '/expenses_controllers', type: :request do
   before(:each) do
     @user = User.create(name: 'Ali', email: 'ali@gmail.com', password: '123456', password_confirmation: '123456')
     @group = Group.create(user_id: @user.id, name: 'Travel', icon: '✈️')
@@ -8,15 +8,15 @@ RSpec.describe "/expenses_controllers", type: :request do
     GroupExpense.create(group_id: @group.id, expense_id: @expense.id)
   end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       get expenses_path
       expect(response).to be_successful
     end
   end
 
-  describe "GET /new" do
-    it "renders a successful response" do
+  describe 'GET /new' do
+    it 'renders a successful response' do
       get new_expense_path
       expect(response).to be_successful
     end
--- spec/requests/groups_controllers_spec.rb
+++ spec/requests/groups_controllers_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "/groups_controllers", type: :request do
-
+RSpec.describe '/groups_controllers', type: :request do
   before(:each) do
     @user = User.create(name: 'Ali', email: 'ali@gmail.com', password: '123456', password_confirmation: '123456')
     Group.create(user_id: @user.id, name: 'Travel', icon: '✈️')
   end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       get groups_path
       expect(response).to be_successful
     end
@@ -24,11 +23,10 @@ RSpec.describe "/groups_controllers", type: :request do
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       get groups_path(@group)
       expect(response).to be_successful
     end
   end
-
 end
