a6211cf518fda04349ce40f98fba692523520820
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-09-11 08:31:32 +0430

    Fix sign up path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_groups)
     groups_path
   end
+  
+  def after_sign_up_path_for(_groups)
+    groups_path
+  end
 end
