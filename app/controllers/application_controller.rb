cf0e01dcafd6f13e14555c5d6ef5c9fcf391e738
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-09-19 12:45:25 +0430

    Fix sign up error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!, except: :welcome
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: :welcome
 
   def after_sign_in_path_for(_groups)
     groups_path
@@ -7,5 +8,12 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(_groups)
     groups_path
+  end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password)
+    end
   end
 end
